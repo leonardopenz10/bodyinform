6aa390b3e91051814d9cc5dcaba6832fe293a661
Leonardo Penz <59891933+leonardopenz10@users.noreply.github.com>
2023-11-21 01:27:22 +0000

    adicionado os arquivos `database.js`e `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,7 @@
+import database from "../../../../infra/database.js";
+
 function status(request, response) {
+  const result = database.query("SELECT 1 + 1");
   response.status(200).json({ chave: "valor" });
 }
 
