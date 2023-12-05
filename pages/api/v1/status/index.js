5e8687cd5aa3eb3d11343af6643795b46e420fc1
Leonardo Penz <59891933+leonardopenz10@users.noreply.github.com>
2023-12-05 15:59:51 +0000

    ajuste no arquivo `page\api\v1\status\index.js`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,7 @@
 import database from "infra/database.js";
 
-function status(request, response) {
-  const result = database.query("SELECT 1 + 1");
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1");
   response.status(200).json({ chave: "valor" });
 }
 
